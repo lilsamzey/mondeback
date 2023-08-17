3607dd41bf9bbecbf824bc101189b5ec73d57976
ali ozkara <lilsamzey@example.com>
2023-08-17 21:56:28 +0200

    aws

--- config.js
+++ config.js
@@ -26,28 +26,28 @@
 
 
 
-// module.exports = {
-//   server: "coursemanagementsystem.cfgtwzvplpnc.eu-north-1.rds.amazonaws.com",
-//       port: 1433,
-//       database: "EdnoSchoolManagementSystem",
-//       user: "admin", 
-//       password: "Klmn-32553255", 
-//       options: {
-//         encrypt: true,
-//         trustServerCertificate: true,
-//       },
-// };
-
-
-
-
 module.exports = {
-  user: 'systemadmin',
-  password: 'Klmn-32553255',
-  server: 'edno.database.windows.net', // Doğru server adresini buraya girin
-  database: 'EdnoSchoolManagementSystem',
-  options: {
-    encrypt: true,
-    trustServerCertificate: true,
-  },
+  server: "coursemanagementsystem.cfgtwzvplpnc.eu-north-1.rds.amazonaws.com",
+      port: 1433,
+      database: "EdnoSchoolManagementSystem",
+      user: "admin", 
+      password: "Klmn-32553255", 
+      options: {
+        encrypt: true,
+        trustServerCertificate: true,
+      },
 };
+
+
+
+
+// module.exports = {
+//   user: 'systemadmin',
+//   password: 'Klmn-32553255',
+//   server: 'edno.database.windows.net', // Doğru server adresini buraya girin
+//   database: 'EdnoSchoolManagementSystem',
+//   options: {
+//     encrypt: true,
+//     trustServerCertificate: true,
+//   },
+// };
